26bc24856d667d9ae5364d4f8e7ec70d236ba6d4
pasignature <pasignature@gmail.com>
2020-05-19 14:35:07 +0000

    commit message

---
+++ brainfuck/README.md
@@ -0,0 +1,58 @@
+## Brainf**k
+
+Brainfuck is an esoteric programming language created in 1993 by
+Urban Müller, and notable for its extreme minimalism
+
+The language consists of only eight simple commands and an
+instruction pointer. While it is fully Turing-complete, it is not
+intended for practical use, but to challenge and amuse programmers.
+Brainfuck simply requires one to break commands into microscopic steps.
+
+### Usage
+
+```sudo apt-get install bf```
+
+**Exercises**
+
+**16. Holberton**
+
+      Write a Brainf*ck script that prints Holberton, followed by
+      a new line.
+
+```
+Usage:
+user@ubuntu:bf$ bf 1000-holberton.bf
+Holberton
+user@ubuntu:bf$
+```
+
+**17. Add two digits**
+
+      Add two digits given by the user.
+```
+Usage:
+user@ubunt:bf$ bf ./1001-add.bf
+81
+9user@ubunt:bf$
+```
+
+**18. Multiplication**
+
+      Multiply two digits given by the user.
+
+* Read the two digits from stdin, multiply them, and print the result
+* The result of the multiplication will be one digit-long (<10)
+```
+Usage:
+user@ubunt:bf$ bf 1002-mul.bf
+24
+8user@ubunt:bf$
+```
+
+**19. Multiplication level up**
+
+      Multiply two digits given by the user.
+
+
+### AUTHORS
+Andrew Godwin
---
+++ monty.h
@@ -0,0 +1,98 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/**
+ * Libaries
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define delim "\n\r\t "
+#define UNUSED(x) (void)(x)
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/* Variables */
+/**
+ * struct variables - global variables to be used
+ * @lineptr: lineptr for getline
+ * @line_number: returns what line_number is getline reading
+ * @value: string value of second argument in file
+ * @int_val: convering value to int
+ * @cmd: string value of first argument in file
+ * @ret_val: return value for main file
+ */
+typedef struct variables
+{
+	char *lineptr;
+	unsigned int line_number;
+	char *value;
+	int int_val;
+	char *cmd;
+	int ret_val;
+	char *flag;
+} vars;
+
+/* global variables */
+extern vars gvars;
+
+/* Operators */
+void op_push(stack_t **stack, unsigned int line_number);
+void op_pall(stack_t **stack, unsigned int line_number);
+void op_pint(stack_t **stack, unsigned int line_number);
+void op_pop(stack_t **stack, unsigned int line_number);
+void op_swap(stack_t **stack, unsigned int line_number);
+void op_add(stack_t **stack, unsigned int line_number);
+void op_nop(stack_t **stack, unsigned int line_number);
+void op_sub(stack_t **stack, unsigned int line_number);
+void op_mul(stack_t **stack, unsigned int line_number);
+void op_pchar(stack_t **stack, unsigned int line_number);
+void op_mod(stack_t **stack, unsigned int line_number);
+void op_div(stack_t **stack, unsigned int line_number);
+void op_pstr(stack_t **stack, unsigned int line_number);
+void op_rotl(stack_t **stack, unsigned int line_number);
+void op_rotr(stack_t **stack, unsigned int line_number);
+void op_queue(stack_t **stack, unsigned int line_number);
+void op_stack(stack_t **stack, unsigned int line_number);
+
+/* Helper */
+void free_l(stack_t *head);
+int find_op(stack_t **head);
+int tokenizer(char *str);
+int check_val(char *str);
+int list_len(stack_t **h);
+int newline_check(char *str);
+
+#endif /* MONTY_H */
